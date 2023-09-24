b552ba597efc83682e1f0e7ac9b9d70fa6f6b150
Edward Wang <edward.z.wang@gmail.com>
2023-09-24 15:40:40 -0400

    More edits

--- scripts.js
+++ scripts.js
@@ -1,18 +1,9 @@
-// Make the FAQ items clickable to show/hide their answers.
-document.querySelectorAll('.faq-item h4').forEach(item => {
-    item.addEventListener('click', event => {
-        const faqItem = event.target.parentElement;
-        if (faqItem.classList.contains('active')) {
-            faqItem.classList.remove('active');
-        } else {
-            faqItem.classList.add('active');
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    const faqToggles = document.querySelectorAll('.faq-toggle');
+    faqToggles.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            const faqItem = this.parentElement;
+            faqItem.classList.toggle('active');
+        });
     });
-});
-
-// Logic for the "See More" link could go here.
-document.querySelector('.see-more-link').addEventListener('click', event => {
-    event.preventDefault();
-    // Example: Open a modal with more FAQs.
-    // This would require additional logic and elements.
 });
