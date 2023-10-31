a6aa5d3ad8653ff48ae90fc22a08ccaf2d0d0232
Edward Wang <edward.z.wang@gmail.com>
2023-10-31 19:09:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,45 +8,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-<script>
-        let currentQuestion = 0;
-        let correctAnswerIndex = 0;
-        let vocabulary = [
-            { word: "Abject", definition: "Of the most contemptible kind", choices: ["Abject", "Pulchritude", "Ineffable", "Equivocate"] },
-            { word: "Cacophony", definition: "Loud confusing disagreeable sounds", choices: ["Garrulous", "Cacophony", "Mellifluous", "Aplomb"] },
-            // Add more vocabulary words here...
-        ];
 
-        function loadQuestion() {
-            if (currentQuestion < vocabulary.length) {
-                let question = vocabulary[currentQuestion];
-                document.getElementById("definition").innerText = question.definition;
-
-                correctAnswerIndex = Math.floor(Math.random() * 4); // Randomly assign correct answer position
-                let j = 0;
-                for (let i = 0; i < 4; i++) {
-                    if (i === correctAnswerIndex) {
-                        document.getElementById(`choice${i}`).innerText = question.word;
-                    } else {
-                        if (j === correctAnswerIndex) j++; // Skip the correct answer in the choices
-                        document.getElementById(`choice${i}`).innerText = question.choices[j++];
-                    }
-                }
-            } else {
-                document.getElementById("definition").innerText = "Quiz finished!";
-                document.querySelector(".choices").style.display = "none";
-            }
-        }
-
-        function checkAnswer(choice) {
-            if (choice === correctAnswerIndex) {
-                document.getElementById("feedback").innerText = "Correct!";
-            } else {
-                document.getElementById("feedback").innerText = "Wrong. Try again!";
-            }
-            currentQuestion++;
-            setTimeout(loadQuestion, 2000); // Load the next question after 2 seconds
-        }
-
-        loadQuestion(); // Load the first question
-    </script>
